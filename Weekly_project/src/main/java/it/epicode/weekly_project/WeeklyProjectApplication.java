c3dff0c0e3e69c0a8de1ad63ae23d1908cf51bf5
Antonio D'Amico <andam@hotmail.it>
2023-04-21 09:14:33 +0200

    first commit

---
+++ Weekly_project/src/main/java/it/epicode/weekly_project/WeeklyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.weekly_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeklyProjectApplication.class, args);
+	}
+
+}
